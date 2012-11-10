eb45c6849b4553225e4562c4efe45fab9d58b93e
Kevin Jett <kevjett@gmail.com>
2012-11-10 16:17:29 -0600

    render module

--- debugBar.render.js
+++ debugBar.render.js
@@ -5,7 +5,7 @@
 
 	debugBar.register('render', function() {
 		var t = performance.timing;
-        var start = t.redirectStart === 0 ? t.fetchStart : t.redirectStart;
+        var start = t.navigationStart;
         var end = t.loadEventEnd;
         var totalTime = (end - start);
 
@@ -18,25 +18,26 @@
 		list.push('DOM: ' + String(t.domComplete - t.domLoading) + "ms");
 		list.push('Load: ' + String(t.loadEventEnd - t.loadEventStart) + "ms");
 
-		var network = { fontColor:'#EA8C7A', barColor:'#FCEEEC', borderColor:'#F6CBC4', sectionColor:'#F4C0B7', itemColor:'#E0543F' };
-		var server = { fontColor:'#FFC933', barColor:'#FFF9E6', borderColor:'#FFEBB2', sectionColor:'#FFE7A1', itemColor:'#FFCB39' };
-		var browser = { fontColor:'#51C2BE', barColor:'#E7F7F7', borderColor:'#83D5D4', sectionColor:'#A6E1E0', itemColor:'#35BAB8' };
+		var network = { sectionStart:0, sectionLength:(t.connectEnd-t.navigationStart), fontColor:'#EA8C7A', barColor:'#FCEEEC', borderColor:'#F6CBC4', sectionColor:'#F4C0B7', itemColor:'#E0543F' };
+		var server = { sectionStart:(network.sectionStart+network.sectionLength), sectionLength:(t.responseEnd-t.requestStart), fontColor:'#FFC933', barColor:'#FFF9E6', borderColor:'#FFEBB2', sectionColor:'#FFE7A1', itemColor:'#FFCB39' };
+		var browser = { sectionStart:(server.sectionStart+server.sectionLength), sectionLength:(end - (server.sectionStart+server.sectionLength)), fontColor:'#51C2BE', barColor:'#E7F7F7', borderColor:'#83D5D4', sectionColor:'#A6E1E0', itemColor:'#35BAB8' };
 
-		var addRow = function(type)
+		var addRow = function(name, value, type, itemLength)
 		{
 			var row = [];
+			itemLength = 10;
 
 			row.push('<div style="display:table;width:100%;color:' + type.fontColor + ';padding:2px;">');
 			row.push('	<div style="display:table-cell;background-color:' + type.barColor + ';border:1px solid ' + type.borderColor + ';">');
-			row.push('		<div style="width:10%;background-color:' + type.sectionColor + ';">');
-			row.push('			<div style="width:10%;background-color:' + type.itemColor + ';height:1.8em;"></div>');
+			row.push('		<div style="margin-left:' + type.sectionStart + '%;width:' + type.sectionLength + '%;background-color:' + type.sectionColor + ';">');
+			row.push('			<div style="width:' + itemLength + '%;background-color:' + type.itemColor + ';height:1.8em;"></div>');
 			row.push('		</div>');
 			row.push('	</div>');
 			row.push('	<div style="display:table-cell;width:70px;padding-left:8px;position:relative;">');
-			row.push('		<span style="top:50%;margin-top:-0.5em;position:absolute">0ms</span>');
+			row.push('		<span style="top:50%;margin-top:-0.5em;position:absolute">' + value + '</span>');
 			row.push('	</div>');
 			row.push('	<div style="display:table-cell;width:200px;position:relative;">');
-			row.push('		<span style="top:50%;margin-top:-0.5em;position:absolute">navigationStart</span>');
+			row.push('		<span style="top:50%;margin-top:-0.5em;position:absolute">' + name + '</span>');
 			row.push('	</div>');
 			row.push('</div>');
 
@@ -50,9 +51,7 @@
 		html.push('			Page Load Time Breakdown / <span style="color:' + network.itemColor + '">network</span> / <span style="color:' + server.itemColor + '">server</span> / <span style="color:' + browser.itemColor + '">browser</span>');
 		html.push('		</h1>');
 		html.push('	</div>');
-		html.push(addRow());
-		html.push('');
-		html.push('');
+		html.push(addRow('Connect', String(t.connectEnd - t.connectStart), network, 0));
 
 		//debugger;
 		return {
@@ -64,6 +63,9 @@
 			},
 			getList : function() {
 				return list;
+			},
+			getContent : function() {
+				return html.join('');
 			}
 		};
 	});
