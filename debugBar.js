43bcd66f473c71cffe67a1a3d92feb6c59a52118
Adam Reeves <dark.account@gmail.com>
2012-11-10 11:55:33 -0600

    more structure

--- debugBar.js
+++ debugBar.js
@@ -1,1 +1,14 @@
-document.body.style.backgroundColor='red';
+/*jshint browser:true, eqnull:true */
+/*global jQuery:false */
+(function(document, $) {
+	'use strict';
+
+	//	quiet stop
+	if (!$ || document.debugBar) return;
+
+	//...
+	document.body.style.backgroundColor='red';
+
+
+
+}(document, window.jQuery));
--- tabLink.js
+++ tabLink.js
@@ -1,4 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	chrome.tabs.insertCSS(tab.id, {file: "style.css"});
+	chrome.tabs.executeScript(tab.id, {file: "jquery.js"});
 	chrome.tabs.executeScript(tab.id, {file: "debugBar.js"});
 });
