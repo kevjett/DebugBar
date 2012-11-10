7877db3c6db08a802a880583edd8f0305d0b8cf6
Kevin Jett <kevjett@gmail.com>
2012-11-10 15:55:29 -0600

    add table for render graph

--- debugBar.render.js
+++ debugBar.render.js
@@ -18,6 +18,42 @@
 		list.push('DOM: ' + String(t.domComplete - t.domLoading) + "ms");
 		list.push('Load: ' + String(t.loadEventEnd - t.loadEventStart) + "ms");
 
+		var network = { fontColor:'#EA8C7A', barColor:'#FCEEEC', borderColor:'#F6CBC4', sectionColor:'#F4C0B7', itemColor:'#E0543F' };
+		var server = { fontColor:'#FFC933', barColor:'#FFF9E6', borderColor:'#FFEBB2', sectionColor:'#FFE7A1', itemColor:'#FFCB39' };
+		var browser = { fontColor:'#51C2BE', barColor:'#E7F7F7', borderColor:'#83D5D4', sectionColor:'#A6E1E0', itemColor:'#35BAB8' };
+
+		var addRow = function(type)
+		{
+			var row = [];
+
+			row.push('<div style="display:table;width:100%;color:' + type.fontColor + ';padding:2px;">');
+			row.push('	<div style="display:table-cell;background-color:' + type.barColor + ';border:1px solid ' + type.borderColor + ';">');
+			row.push('		<div style="width:10%;background-color:' + type.sectionColor + ';">');
+			row.push('			<div style="width:10%;background-color:' + type.itemColor + ';height:1.8em;"></div>');
+			row.push('		</div>');
+			row.push('	</div>');
+			row.push('	<div style="display:table-cell;width:70px;padding-left:8px;position:relative;">');
+			row.push('		<span style="top:50%;margin-top:-0.5em;position:absolute">0ms</span>');
+			row.push('	</div>');
+			row.push('	<div style="display:table-cell;width:200px;position:relative;">');
+			row.push('		<span style="top:50%;margin-top:-0.5em;position:absolute">navigationStart</span>');
+			row.push('	</div>');
+			row.push('</div>');
+
+			return row.join('');
+		};
+
+		var html = [];
+		html.push('<div style="font-size: 12px; line-height: 1em;  text-align: left; font-family: Helvetica, Calibri, Arial, sans-serif; text-shadow: none;  display: inline-block; color: rgb(34, 34, 34); font-weight: normal; border: none; margin: 0px auto; padding: 10px; background-color: rgba(255, 253, 242, 0.952941); width: 95%; position: fixed; background-position: initial initial; background-repeat: initial initial;">');
+		html.push('	<div>');
+		html.push('		<h1 style="font-size: 24px; line-height: 1em; z-index: 999; text-align: left; font-family: Helvetica, Calibri, Arial, sans-serif; text-shadow: none; box-shadow: none; display: inline-block; color: rgb(34, 34, 34); font-weight: normal; border: none; margin: 10px 0px; padding: 0px; background-image: none; width: auto; background-position: initial initial; background-repeat: initial initial;">');
+		html.push('			Page Load Time Breakdown / <span style="color:' + network.itemColor + '">network</span> / <span style="color:' + server.itemColor + '">server</span> / <span style="color:' + browser.itemColor + '">browser</span>');
+		html.push('		</h1>');
+		html.push('	</div>');
+		html.push(addRow());
+		html.push('');
+		html.push('');
+
 		//debugger;
 		return {
 			getName : function () {
